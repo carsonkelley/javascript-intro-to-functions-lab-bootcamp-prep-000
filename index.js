58d9723a053b6abc38071e9658e709f71cde3b28
Carson Kelley <carsonmkelley@gmail.com>
2018-06-09 22:27:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,5 +13,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  
+  if(string === string.toLowerCase()) {
+    return "I can't hear you!"
+  }
 }
